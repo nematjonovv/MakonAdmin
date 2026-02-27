e805e40451239be663e9c31d426eada72ba30182
Hikmatillo <hikmatillonematjonovv19@gmail.com>
2026-02-27 15:56:58 +0500

    fix: set secure cookie settings for production https

--- server/src/scripts/setAuthCookies.ts
+++ server/src/scripts/setAuthCookies.ts
@@ -3,8 +3,8 @@ import { Response } from "express";
 export async function SetAuthCookie(res: Response, token: string) {
   res.cookie("access_token", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });

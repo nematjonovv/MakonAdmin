64b0151c9d46a21ff25cf732199c2adf31d967ea
Hikmatillo <hikmatillonematjonovv19@gmail.com>
2026-02-27 07:13:25 +0500

    connect supabase

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,17 +1,4 @@
 import "dotenv/config";
-// import { PrismaClient } from "../generated/prisma";
-
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "makon_admin",
-});
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = new PrismaClient();

6b54346b3bd98e53e8b66d8c4d02de7d8986299d
Hikmatillo <hikmatillonematjonovv19@gmail.com>
2026-02-27 18:31:26 +0500

    nextcondfig

--- admin-ui/next.config.ts
+++ admin-ui/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://makon.onrender.com/:path*",
+      },
+    ];
+  },  
 };
 
 export default nextConfig;
